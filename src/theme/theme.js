ff203c54c562ca59c16abff516a085b779d9b88f
arturluis <arturluisoliveira@gmail.com>
2020-09-15 20:01:09 -0300

    add boilerplate

---
+++ src/theme/theme.js
@@ -0,0 +1,6 @@
+// Base variables: https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+
+module.exports = {
+    'primary-color': '#fcba03',
+    'link-color': '#fcba03'
+};
